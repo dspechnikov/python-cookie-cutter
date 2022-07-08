8448d37b8b3c7616ab220fc2b49d1fcc2546d54e
Dmitry Pechnikov <dpechnikov@pm.me>
2022-07-08 11:39:45 +0300

    Use cookiecutter for easier templating and better UI

--- setup.py
+++ setup.py
@@ -1,42 +1,8 @@
-import os
 import shlex
-import shutil
 import subprocess
 import sys
-from pathlib import Path
-from string import Template
 
 TEMPLATE_REPO_URL = "https://github.com/dspechnikov/python-cookie-cutter"
-DEFAULT_PROJECT_NAME = "python_template"
-DEFAULT_PYTHON_VERSION = "3.10"
-BASE_DIR = Path(os.getcwd())
-GITHUB_WORKFLOWS_DIR = Path(".github/workflows")
-TEMPLATES_DIR_NAME = "templates"
-
-PYTHON_VERSION_TEMPLATES = (
-    ("Pipfile.tpl", "Pipfile"),
-    (".pre-commit-config.yaml.tpl", ".pre-commit-config.yaml"),
-    ("production.yml.tpl", Path(".github/workflows") / "production.yml"),
-    ("staging.yml.tpl", Path(".github/workflows") / "staging.yml"),
-)
-# fmt: off
-SRC_DIR_TEMPLATES = (
-    ("pyproject.toml.tpl", "pyproject.toml"),
-)
-# fmt: on
-
-
-def render_template(template_path, context: dict, out_path=None):
-    template_dir = Path(template_path).parent
-
-    with open(template_path) as template_file:
-        template = Template(template_file.read())
-
-    out_file_path = out_path or template_dir / Path(template_path).name.replace(
-        ".tpl", ""
-    )
-    with open(out_file_path, "w") as out_file:
-        out_file.write(template.substitute(**context))
 
 
 def run_cmd(cmd: str, env=None) -> None:
@@ -44,56 +10,8 @@ def run_cmd(cmd: str, env=None) -> None:
 
 
 def main() -> None:
-    project_name = (
-        input(f"Project name ({DEFAULT_PROJECT_NAME}): ") or DEFAULT_PROJECT_NAME
-    )
-    python_version = (
-        input(f"Python version ({DEFAULT_PYTHON_VERSION}): ") or DEFAULT_PYTHON_VERSION
-    )
-
-    project_dir = BASE_DIR / project_name
-    templates_dir = project_dir / TEMPLATES_DIR_NAME
-
-    # clone the repo
-    run_cmd(f"git clone {TEMPLATE_REPO_URL} {project_name}")
-
-    # prepare directory structure
-    os.rename(project_dir / "src", project_dir / project_name)
-    (project_dir / GITHUB_WORKFLOWS_DIR).mkdir(parents=True, exist_ok=True)
-
-    os.chdir(project_dir)
-
-    # create config files
-    for template, out_path in PYTHON_VERSION_TEMPLATES:
-        render_template(
-            templates_dir / template,
-            context={"python_version": python_version},
-            out_path=out_path,
-        )
-
-    for template, out_path in SRC_DIR_TEMPLATES:
-        render_template(
-            templates_dir / template,
-            context={"src_dir": project_name},
-            out_path=out_path,
-        )
-
-    # install dependencies
-    run_cmd(
-        "pipenv install -d",
-        env={
-            **os.environ,
-            "PIPENV_VENV_IN_PROJECT": "1",
-            "PIPENV_IGNORE_VIRTUALENVS": "1",
-        },
-    )
-
-    # install pre-commit hooks
-    run_cmd("pre-commit install")
-
-    # remove files needed only for setup
-    shutil.rmtree(templates_dir)
-    os.remove(project_dir / "setup.py")
+    run_cmd("pip install -q pipenv cookiecutter")
+    run_cmd(f"cookiecutter {TEMPLATE_REPO_URL}")
 
 
 if __name__ == "__main__":
