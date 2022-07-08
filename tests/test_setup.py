e374bdb85a7077a944649d22039a4128c0bf7332
Dmitry Pechnikov <dpechnikov@pm.me>
2022-07-08 12:24:01 +0300

    Remove unused code from tests

--- tests/test_setup.py
+++ tests/test_setup.py
@@ -1,13 +1,7 @@
 import os
-import shlex
-import subprocess
 from pathlib import Path
 
 from cookiecutter.main import cookiecutter
-
-
-def run_cmd(cmd: str, env=None) -> None:
-    subprocess.run(shlex.split(cmd), env=env, check=True)
 
 
 def test_setup():
