376707513a41588393fbb3a920b74ca0a1fac8ed
Dmitry Pechnikov <dpechnikov@pm.me>
2022-07-12 09:54:14 +0300

    Fix invalid package name if project name has dashes
    Replace dashes with underscores.

--- tests/test_setup.py
+++ tests/test_setup.py
@@ -1,10 +1,21 @@
-import os
+import shutil
 from pathlib import Path
 
 from cookiecutter.main import cookiecutter
 
 
 def test_setup():
-    cookiecutter(".", no_input=True, overwrite_if_exists=True)
+    current_dir = Path(__file__).parent
+    out_path = current_dir / "python_template"
 
-    assert (Path(os.getcwd()) / "python_template").exists()
+    cookiecutter(
+        template=str(current_dir.parent),
+        no_input=True,
+        overwrite_if_exists=True,
+        accept_hooks=False,
+        output_dir=out_path,
+    )
+
+    assert out_path.exists()
+
+    shutil.rmtree(out_path)
